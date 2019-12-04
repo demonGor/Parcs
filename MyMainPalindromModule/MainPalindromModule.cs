9af075b19b694b9360bce44efd1b449d4e790ae6
Dmytro Zazymko <dmytro_zazymko@epam.com>
2019-12-05 00:13:32 +0200

    initial

---
+++ MyMainPalindromModule/MainPalindromModule.cs
@@ -0,0 +1,68 @@
+﻿using Parcs;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+
+namespace MyMainPalindromModule
+{
+    class MainPalindromModule : IModule
+    {
+        static void Main(string[] args)
+        {
+            var job = new Job();
+            if (!job.AddFile(Assembly.GetExecutingAssembly().Location/*"MyMainPalindromModule.exe"*/))
+            {
+                Console.WriteLine("File doesn't exist");
+                return;
+            }
+
+            (new MainPalindromModule()).Run(new ModuleInfo(job, null));
+            Console.ReadKey();
+        }
+
+        public void Run(ModuleInfo info, CancellationToken token = default(CancellationToken))
+        {
+            Console.WriteLine("Enter left border a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter right border b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            const int pointsNum = 2;
+            var points = new IPoint[pointsNum];
+            var channels = new IChannel[pointsNum];
+            for (int i = 0; i < pointsNum; ++i)
+            {
+                points[i] = info.CreatePoint();
+                channels[i] = points[i].CreateChannel();
+                points[i].ExecuteClass("MyMainPalindromModule.PalindromModule");
+            }
+
+            int y = a;
+            int c = a;
+            for (int i = 0; i < pointsNum; ++i)
+            {
+                channels[i].WriteData(c);
+                channels[i].WriteData(Math.Min(y + (int)Math.Floor((double)(b - a) / pointsNum),b));
+                y += (int)Math.Floor((double)(b - a) / pointsNum);
+                c = y+1;
+            }
+            DateTime time = DateTime.Now;
+            Console.WriteLine("Waiting for result...");
+
+            List<int> res = new List<int>();
+            for (int i = 0; i < pointsNum; ++i)
+            {
+                res.AddRange(channels[i].ReadObject<List<int>>());
+            }
+
+            foreach (var number in res)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Number of simple palindroms: {0}, time = {1}", res.Count, Math.Round((DateTime.Now - time).TotalSeconds, 3));
+
+        }
+    }
+}
---
+++ MyMainPalindromModule/PalindromModule.cs
@@ -0,0 +1,65 @@
+﻿using Parcs;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MyMainPalindromModule
+{
+    class PalindromModule : IModule
+    {
+        private static List<int> SimplePalindrom(int a, int b)
+        {
+            List<int> res = new List<int>();
+            for (int j = a; j <= b; ++j)
+            {
+                if(IsSimple(j) && IsPalindrom(j))
+                {
+                    res.Add(j);
+                }
+            }
+
+            return res;
+        }
+
+        private static bool IsSimple(int n)
+        {
+            if (n == 1) return false;
+
+            int k = 0;
+
+            for (int i=2;i<=(int)Math.Sqrt(n); ++i)
+            {
+                if (n % i == 0) { k += 1; }
+            }
+
+            if (k == 0)
+                return true;
+
+            return false;
+        }
+
+        private static bool IsPalindrom(int n)
+        {
+            string s1 = n.ToString();
+
+            for (int i=0; i<s1.Length/2; ++i)
+            {
+                if (s1[i] != s1[s1.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void Run(ModuleInfo info, CancellationToken token = default(CancellationToken))
+        {
+            int a = info.Parent.ReadInt();
+            int b = info.Parent.ReadInt();
+           
+            List<int> res = SimplePalindrom(a, b);
+            info.Parent.WriteObject(res);
+        }
+    }
+}
